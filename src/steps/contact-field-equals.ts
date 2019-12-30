167748068efd8057d444c87f7c5a4c4030ff4921
Eric Peterson <i.am@eric.pe>
2019-12-30 15:43:56 -0800

    Update Contact Field Check step with util.compare method and operators.

--- src/steps/contact-field-equals.ts
+++ src/steps/contact-field-equals.ts
@@ -3,6 +3,7 @@
 
 import { BaseStep, Field, StepInterface } from '../core/base-step';
 import { FieldDefinition, RunStepResponse, Step, StepDefinition } from '../proto/cog_pb';
+import * as util from '@run-crank/utilities';
 
 /**
  * Note: the class name here becomes this step's stepId.
@@ -26,7 +27,16 @@ export class ContactFieldEquals extends BaseStep implements StepInterface {
    * named regex capturing groups that correspond to the expected fields below.
    */
   // tslint:disable-next-line:max-line-length
-  protected stepExpression: string = 'the (?<field>.+) field on eloqua contact (?<email>.+) should be (?<expectedValue>.+)';
+  protected stepExpression: string = 'the (?<field>.+) field on eloqua contact (?<email>.+) should (?<operator>be less than|be greater than|be|contain|not be|not contain) (?<expectedValue>.+)';
+
+  protected supportedOperators: string[] = [
+    'be',
+    'not be',
+    'contain',
+    'not contain',
+    'be less than',
+    'be greater than',
+  ];
 
   /**
    * An array of Fields that this step expects to be passed via step data. The value of "field"
@@ -41,6 +51,11 @@ export class ContactFieldEquals extends BaseStep implements StepInterface {
     type: FieldDefinition.Type.STRING,
     description: 'Field name to check',
   }, {
+    field: 'operator',
+    type: FieldDefinition.Type.STRING,
+    optionality: FieldDefinition.Optionality.OPTIONAL,
+    description: 'Check Logic (be, not be, contain, not contain, be greater than, or be less than)',
+  }, {
     field: 'expectedValue',
     type: FieldDefinition.Type.ANYSCALAR,
     description: 'Expected field value',
@@ -51,33 +66,46 @@ export class ContactFieldEquals extends BaseStep implements StepInterface {
     const stepData: any = step.getData().toJavaScript();
     const email: string = stepData.email;
     const field: string = stepData.field;
+    const operator: string = (stepData.operator || 'be').toLowerCase();
     const expectedValue: string = stepData.expectedValue;
 
     try {
       apiRes = await this.client.searchContactsByEmail(email);
     } catch (e) {
-      return this.error('There was a problem connecting to JSON Placeholder.');
+      return this.error('There was a problem connecting to Eloqua: %s', [
+        e.toString(),
+      ]);
     }
 
-    if (apiRes.elements.length === 0) {
-      // If no results were found, return an error.
-      return this.error('No contact found for email %s', [email]);
-    } else if (!apiRes.elements[0].hasOwnProperty(field)) {
-      // If the given field does not exist on the user, return an error.
-      return this.error('The %s field does not exist on contact %s', [field, email]);
-    } else if (apiRes.elements[0][field] == expectedValue) {
-      // If the value of the field matches expectations, pass.
-      return this.pass('The %s field was set to %s, as expected', [
-        field,
-        apiRes.elements[0][field],
-      ]);
-    } else {
-      // If the value of the field does not match expectations, fail.
-      return this.fail('Expected %s field to be %s, but it was actually %s', [
-        field,
-        expectedValue,
-        apiRes.elements[0][field],
-      ]);
+    try {
+      if (apiRes.elements.length === 0) {
+        // If no results were found, return an error.
+        return this.error('No contact found for email %s', [email]);
+      } else if (!apiRes.elements[0].hasOwnProperty(field)) {
+        // If the given field does not exist on the contact, return an error.
+        return this.error('The %s field does not exist on contact %s', [field, email]);
+      } else if (util.compare(operator, apiRes.elements[0][field], expectedValue)) {
+        // If the value of the field meets expectations, pass.
+        return this.pass(util.operatorSuccessMessages[operator], [
+          field,
+          expectedValue,
+        ]);
+      } else {
+        // If the value of the field does not match expectations, fail.
+        return this.fail(util.operatorFailMessages[operator], [
+          field,
+          expectedValue,
+          apiRes.elements[0][field],
+        ]);
+      }
+    } catch (e) {
+      if (e instanceof util.UnknownOperatorError) {
+        return this.error('%s Please provide one of: %s', [e.message, this.supportedOperators.join(', ')]);
+      }
+      if (e instanceof util.InvalidOperandError) {
+        return this.error(e.message);
+      }
+      return this.error('There was an error checking the contact field: %s', [e.message]);
     }
   }
 
